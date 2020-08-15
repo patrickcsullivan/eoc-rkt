b62795078764fe2ecc786ce0361330a19cddcd0e
Patrick Sullivan <patricksullivan08@gmail.com>
2020-08-14 21:49:42 -0400

    wip

--- compiler.rkt
+++ compiler.rkt
@@ -11,44 +11,40 @@
 ;; R0 examples
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; The following compiler pass is just a silly one that doesn't change
-;; anything important, but is nevertheless an example of a pass. It
-;; flips the arguments of +. -Jeremy
-(define (flip-exp e)
-  (match e
-    [(? fixnum?) e]
-    [`(read) `(read)]
-    [`(- ,e1) `(- ,(flip-exp e1))]
-    [`(+ ,e1 ,e2) `(+ ,(flip-exp e2) ,(flip-exp e1))]
-    ))
-
-(define (flip-R0 e)
-  (match e
-    [`(program ,e) `(program ,(flip-exp e))]
-    ))
-
-
 ;; Next we have the partial evaluation pass described in the book.
 (define (pe-neg r)
-  (cond [(fixnum? r) (fx- 0 r)]
-	[else `(- ,r)]))
+  (define r (pe-exp r))
+  (cond 
+    [(fixnum? r) 
+      (fx- 0 r)]
+	  [else 
+      `(- ,r)]))
 
-(define (pe-add r1 r2)
-  (cond [(and (fixnum? r1) (fixnum? r2)) (fx+ r1 r2)]
-	[else `(+ ,r1 ,r2)]))
+(define (pe-add r1 r2 with-add)
+  (cond 
+    [(and (fixnum? r1) (fixnum? r2)) 
+      (fx+ r1 r2)]
+	  [else 
+      `(+ ,r1 ,r2)]))
 
 (define (pe-exp e)
   (match e
-    [(? fixnum?) e]
-    [`(read) `(read)]
-    [`(- ,e1) (pe-neg (pe-exp e1))]
-    [`(+ ,e1 ,e2) (pe-add (pe-exp e1) (pe-exp e2))]
+    [(? fixnum?) 
+      e]
+    [`(read) 
+      `(read)]
+    [`(- ,e1) 
+      (pe-neg (pe-exp e1))]
+    [`(+ ,e1 ,e2) 
+      (pe-add (pe-exp e1) (pe-exp e2))]
     ))
 
 (define (pe-R0 p)
   (match p
     [`(program ,e) `(program ,(pe-exp e))]
     ))
+
+(define my-test `(program (+ 1 (+ (read) 1))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HW1 Passes
