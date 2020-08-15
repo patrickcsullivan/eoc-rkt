e33ec41a352a02c9b517331ca2e75dbb07392a8b
Patrick Sullivan <patricksullivan08@gmail.com>
2020-08-14 21:52:36 -0400

    wip

--- compiler.rkt
+++ compiler.rkt
@@ -8,45 +8,6 @@
 (provide (all-defined-out))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; R0 examples
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Next we have the partial evaluation pass described in the book.
-(define (pe-neg r)
-  (define r (pe-exp r))
-  (cond 
-    [(fixnum? r) 
-      (fx- 0 r)]
-	  [else 
-      `(- ,r)]))
-
-(define (pe-add r1 r2 with-add)
-  (cond 
-    [(and (fixnum? r1) (fixnum? r2)) 
-      (fx+ r1 r2)]
-	  [else 
-      `(+ ,r1 ,r2)]))
-
-(define (pe-exp e)
-  (match e
-    [(? fixnum?) 
-      e]
-    [`(read) 
-      `(read)]
-    [`(- ,e1) 
-      (pe-neg (pe-exp e1))]
-    [`(+ ,e1 ,e2) 
-      (pe-add (pe-exp e1) (pe-exp e2))]
-    ))
-
-(define (pe-R0 p)
-  (match p
-    [`(program ,e) `(program ,(pe-exp e))]
-    ))
-
-(define my-test `(program (+ 1 (+ (read) 1))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HW1 Passes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
