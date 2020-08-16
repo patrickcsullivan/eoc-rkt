374a1d394cc41ff2c7c95e5a6521468550ea9fd6
Patrick Sullivan <patricksullivan08@gmail.com>
2020-08-16 18:29:50 -0400

    remove-complex-opera*

--- compiler.rkt
+++ compiler.rkt
@@ -24,6 +24,8 @@
 ;; unique-symbol-table : HashMap Symbol Symbol
 (define old-to-unique-symbol-table (make-hash))
 
+;;-------------------------------------------------------------------------------
+
 ;; uniquify-exp : R1 -> R1
 (define (uniquify-exp e)
   (match e
@@ -52,15 +54,89 @@
      `(program ,info ,(uniquify-exp e))]
     ))
 
-(define my-test
+(define uniquify-test
   `(let ([my-var 42])
         (let ([input (read)])
              (let ([my-var (+ my-var (- input))])
                   my-var))))
 
+;;-------------------------------------------------------------------------------
+
+;; complex-opera? : R1 -> Boolean
+(define (complex-opera? e)
+  (match e
+    [(? symbol?) #f]
+    [(? integer?) #f]
+    [else #t]))
+
+;; simplify an operator with one argument
+;; simplify-operator1 : Operator -> R1 -> R1
+(define (simplify-operator1 op arg)
+  (match (complex-opera? arg)
+    ; assign arg to new var and wrap operator in let
+    [#t
+     (define next-var (next-unique-symbol))
+     (define arg+ (remove-complex-opera-exp* arg))
+     `(let ([,next-var ,arg+]) (,op ,next-var))]
+    ; just return operator with simple arg
+    [else `(,op ,arg)]))
+
+;; simplify an operator with two arguments
+;; simplify-operator1 : Operator -> R1 -> R1 -> R1
+(define (simplify-operator2 op arg1 arg2)
+  (match* ((complex-opera? arg1) (complex-opera? arg2))
+    [(#t #t)
+     (define next-var1 (next-unique-symbol))
+     (define arg1+ (remove-complex-opera-exp* arg1))
+     (define next-var2 (next-unique-symbol))
+     (define arg2+ (remove-complex-opera-exp* arg2))
+     `(let ([,next-var1 ,arg1+]) (let ([,next-var2 ,arg2+]) (,op ,next-var1 ,next-var2)))]
+    [(#t #f)
+     (define next-var (next-unique-symbol))
+     (define arg1+ (remove-complex-opera-exp* arg1))
+     `(let ([,next-var ,arg1+]) (,op ,next-var ,arg2))]
+    [(#f #t)
+     (define next-var (next-unique-symbol))
+     (define arg2+ (remove-complex-opera-exp* arg2))
+     `(let ([,next-var ,arg2+]) (,op ,arg1 ,next-var))]
+    [(#f #f)
+     `(,op ,arg1 ,arg2)]))
+
+;; remove-complex-opera-exp* : R1 -> R1
+(define (remove-complex-opera-exp* e)
+  (match e
+    [(? symbol?) e]
+    [(? integer?) e]
+    [`(let ([,v ,bnd]) ,bdy)
+      (define bnd+ (remove-complex-opera-exp* bnd))
+      (define bdy+ (remove-complex-opera-exp* bdy))
+      `(let ([,v ,bnd+]) ,bdy+)]
+    ; operators
+    [`(read) e]
+    [`(+ ,e1 ,e2) (simplify-operator2 '+ e1 e2)]
+    [`(- ,e) (simplify-operator1 '- e)]
+    ))
+
 ;; remove-complex-opera* : R1 -> R1
 (define (remove-complex-opera* p)
-  p)
+  (match p
+    [`(program ,info ,e)
+     `(program ,info ,(remove-complex-opera-exp* e))]
+    ))
+
+(define remove-complex-test
+  `(+ (+ 1 2) (+ (+ 3 (read)) (+ (read) 4))))
+
+(define remove-complex-expected
+  '(let ((_0 (+ 1 2)))
+     (let ((_1 (let ((_2 (let ((_3 (read)))
+                           (+ 3 _3))))
+                 (let ((_4 (let ((_5 (read)))
+                             (+ _5 4))))
+                   (+ _2 _4)))))
+       (+ _0 _1))))
+
+;;-------------------------------------------------------------------------------
 
 ;; explicate-control : R1 -> C0
 (define (explicate-control p)
